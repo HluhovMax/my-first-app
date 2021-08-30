eec866c177e91e03c0c9f64183a747e17ed2d2fc
Maksym Hlukhov <maksym.hlukhov@bitvoodoo.ch>
2021-08-30 22:47:43 +0300

    Fully Understanding the Component Selector Types (attribute, class, common)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]', <- attribute-like selector
+  // selector: `.app-servers`, <- class-like selector
+  selector: `app-servers`, // <- most common selector type
   template: '<server></server>',
   styleUrls: ['./servers.component.css']
 })
