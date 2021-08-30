8fc393e45eb14c9fc62e39e2910bed2095748342
Maksym Hlukhov <maksym.hlukhov@bitvoodoo.ch>
2021-08-30 23:09:04 +0300

    first task: added 2 components, integrated them to whole app

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]', <- attribute-like selector
-  // selector: `.app-servers`, <- class-like selector
   selector: `app-servers`, // <- most common selector type
   template: '<server></server>',
   styleUrls: ['./servers.component.css']
